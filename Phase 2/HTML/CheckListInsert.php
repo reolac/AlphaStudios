189295524b157fcbe36b781fceb6bc7a1ccf8628
danny95djb <dannybentley@live.com>
2015-04-24 10:58:48 +0100

    CheckList uses prepared statements now
    removed unused variable

--- Phase 2/HTML/CheckListInsert.php
+++ Phase 2/HTML/CheckListInsert.php
@@ -14,12 +14,30 @@ if(mysqli_connect_errno())
 	printf("connection failed :%s\n", mysqli_connect_error());
 	exit();
 }
-$WOR = $_SESSION['WoR'];
 if(isset($_POST['submit']))
 {
-	$query = "INSERT INTO checklist (DrawRefNum, CostingSheet, Quotation, CustPurOrdNum, JobSheet, PurOrdNum, DelNotNum, InvNum, RejNum, TestCertNum, LPTest, PhotoRefNum)
-	VALUES ('$_POST[drawingReferenceNumber]','$_POST[costingSheet]','$_POST[quotation]','$_POST[customPurchaseOrderNumber]','$_POST[jobSheet]','$_POST[purchaseOrderNumber]','$_POST[deliveryNoteNumber]','$_POST[invoiceNumber]','$_POST[rejectNumber]','$_POST[testCertificateNumber]','$_POST[lpTestNumber]','$_POST[photoReferenceNumber]')";
-	mysqli_query($conn,$query);
+	if($stmt = mysqli_prepare($conn, "INSERT INTO checklist (DrawRefNum, CostingSheet, Quotation, CustPurOrdNum, JobSheet, PurOrdNum, DelNotNum, InvNum, RejNum, TestCertNum, LPTest, PhotoRefNum)
+	VALUES(?,?,?,?,?,?,?,?,?,?,?,?)"))
+	{
+		$DrF = $_POST['drawingReferenceNumber'];
+		$CS = $_POST['costingSheet'];
+		$Quo = $_POST['quotation'];
+		$CpoN = $_POST['customPurchaseOrderNumber'];
+		$JS = $_POST['jobSheet'];
+		$PoN = $_POST['purchaseOrderNumber'];
+		$DnN = $_POST['deliveryNoteNumber'];
+		$IN = $_POST['invoiceNumber'];
+		$RN = $_POST['rejectNumber'];
+		$TcN = $_POST['testCertificateNumber'];
+		$LtN = $_POST['lpTestNumber'];
+		$PrN = $_POST['photoReferenceNumber'];
+		mysqli_stmt_bind_param($stmt , 'ssssssssssss', $DrF, $CS, $Quo, $CpoN, $JS, $PoN, $DnN, $IN, $RN, $TcN, $LtN, $PrN);
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_close($stmt);
+	}
+	#$query = "INSERT INTO checklist (DrawRefNum, CostingSheet, Quotation, CustPurOrdNum, JobSheet, PurOrdNum, DelNotNum, InvNum, RejNum, TestCertNum, LPTest, PhotoRefNum)
+	#VALUES ('$_POST[drawingReferenceNumber]','$_POST[costingSheet]','$_POST[quotation]','$_POST[customPurchaseOrderNumber]','$_POST[jobSheet]','$_POST[purchaseOrderNumber]','$_POST[deliveryNoteNumber]','$_POST[invoiceNumber]','$_POST[rejectNumber]','$_POST[testCertificateNumber]','$_POST[lpTestNumber]','$_POST[photoReferenceNumber]')";
+	#mysqli_query($conn,$query);
 	echo "inserted";
 	exit();
 }
