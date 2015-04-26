b6d80a56a286f4bbdca0f65211062cfbb8dfaa18
Michael Smith <reolac@gmail.com>
2015-04-26 20:22:25 +0100

    rowCalc
    also fixed ids being added to td instead of input
    
    to do total calc

--- Phase 2/JS/script.js
+++ Phase 2/JS/script.js
@@ -6,51 +6,49 @@ var row = table.insertRow(-1);
 
 // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
 var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2);
-var cell5 = row.insertCell(3);
-var cell6 = row.insertCell(4);
 
 
-var element2 = document.createElement('input');
-element2.type="text";
 
-var element3 = document.createElement('input');
-element3.type="text";
 
-var element4 = document.createElement('input');
-element4.type="text";
-
-var element5 = document.createElement('input');
-element5.type="text";
-
-cell1.id += tableID + "Ref" + len;
-cell2.id += tableID + "Desc" + len;
-cell3.id += tableID + "Reqd" + len;
-cell5.id += tableID + "UCost" + len;
-cell6.id += tableID + "TCost" + len;
 
 cell1.innerHTML = len;
+cell1.id += tableID + "Ref" + len;
 
-
+var cell2 = row.insertCell(1);
+var element2 = document.createElement('input');
+element2.type="text";
 cell2.appendChild(element2);
+
+var cell3 = row.insertCell(2);
+var element3 = document.createElement('input');
+element3.type="text";
+element3.id = tableID + "Reqd" + len;
+element3.onkeyup=function() {calc(tableID, len);}
 cell3.appendChild(element3);
+
+var cell4 = row.insertCell(3);
+var element4 = document.createElement('input');
+element4.type="text";
+element4.id = tableID + "UCost" + len;
+element4.onkeyup=function() {calc(tableID, len);}
+cell4.appendChild(element4);
+
+
+var cell5 = row.insertCell(4);
+var element5 = document.createElement('input');
+element5.type="text";
+element5.id = tableID + "TCost" + len;
 cell5.appendChild(element5);
-
-
 
 
 // Add some text to the new cells:
 
 }
 
-function getTotal(tableID){
-    var req = document.getElementById(tableID + 'Reqd' + 0);
-    var uCost = document.getElementById(tableID + 'UCost' + 0);
-    
-    
-    var tot=0;
-    tot = parseInt(req * uCost);
-    
-    document.getElementById(tableID + 'TCost' + 0).value = tot;
+function calc(tableID, idx) {
+  var price = parseFloat(document.getElementById(tableID+"Reqd"+idx).value)*
+              parseFloat(document.getElementById(tableID+"UCost"+idx).value);
+  //alert(idx+":"+price);  
+  document.getElementById(tableID+"TCost"+idx).value= isNaN(price)?"0.00":price.toFixed(2);
+   
 }
